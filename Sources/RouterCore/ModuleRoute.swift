bfded3d0d7cc7d79a568d8ade1c2cecf26007516
happyo <xiaxiongzhi@corp.netease.com>
2024-09-04 17:22:39 +0800

    @dev # Add action check

--- Sources/RouterCore/ModuleRoute.swift
+++ Sources/RouterCore/ModuleRoute.swift
@@ -6,10 +6,15 @@ import Foundation
 
 public protocol ModuleRoute: Hashable {
     static func key() -> String
+    static func isAction() -> Bool
 }
 
 public extension ModuleRoute {
     static func key() -> String {
         return String(describing: type(of: self))
+    }
+
+    static func isAction() -> Bool {
+        return false
     }
 }
